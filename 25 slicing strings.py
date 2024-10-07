25c108ad6ea59a7c69d31e89ab622cd7425a8341
Guest1 <alfiyazon@gmail.com>
2024-10-06 23:48:22 -0700

    6-10-2024

---
+++ 25 slicing strings.py
@@ -0,0 +1,13 @@
+# Slicing strings
+
+#You can return a range of characters by using the slicing syntax
+
+#Specify the start index and the end index
+#separated  by a colon, to return  a part of the string.
+
+
+#Get the characters from positioning 2 to position 5 (not included )
+
+b = "Hello, World!"
+print(b[2:5]) #stop index is not added
+print(b[7:12]) # to get world
